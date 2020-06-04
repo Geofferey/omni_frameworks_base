e885e3b3da79de234120d6b99a74a35b25f4a747
Geofferey Eakins <geofferey.eakins@gmail.com>
2020-06-04 01:22:13 -0700

    remove: "Android System Internal problem" warning
    This change will remove the message displayed on lockscreen after boot which states "Android System - There's an internal problem with your device. Contact your manufacturer for details.". This is usually caused by inconsistent build fingerprint between /system & /vendor, but other things may cause it to trip such as custom kernels. The message is essentially useless and tells us nothing, not even if the device will be unstable. A log message should still be available.
    Change-Id: If7aecb75b6bd66c61a1f3179b567114db74b6d0d

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -6487,7 +6487,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
                 if (!Build.isBuildConsistent()) {
                     Slog.e(TAG, "Build fingerprint is not consistent, warning user");
-                    mUiHandler.post(() -> {
+                    /*mUiHandler.post(() -> {
                         if (mShowDialogs) {
                             AlertDialog d = new BaseErrorDialog(mUiContext);
                             d.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ERROR);
@@ -6499,7 +6499,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
                                     mUiHandler.obtainMessage(DISMISS_DIALOG_UI_MSG, d));
                             d.show();
                         }
-                    });
+                    });*/
                 }
             }
         }
